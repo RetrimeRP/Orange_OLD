18b1a01907939fbb6e434e929e573dd0c1bbe908
Refosel <82279481+refoseldev@users.noreply.github.com>
2025-07-02 01:51:23 +0300

    Delete garrysmod/addons/cityworker/lua directory

--- garrysmod/addons/cityworker/lua/entities/cityworker_electric/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type            = "anim"
-ENT.Base            = "base_gmodentity"
-
-ENT.PrintName       = "Problème Électrique"
-ENT.Category        = "City Worker"
-ENT.Author          = "Silhouhat"
-ENT.Contact 	    = ""
-
-ENT.Spawnable   	= false
--- garrysmod/addons/cityworker/lua/entities/cityworker_rubble/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type            = "anim"
-ENT.Base            = "base_gmodentity"
-
-ENT.PrintName       = "Débris"
-ENT.Category        = "City Worker"
-ENT.Author          = "Silhouhat"
-ENT.Contact 	    = ""
-
-ENT.Spawnable   	= false
